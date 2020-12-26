cb01859d9c94bcb3272be371632597d975b46e3b
Hamza Rafi <rafi.hamza2020@gmail.com>
2020-12-26 02:40:15 +0000

    Added GitHub and Discord icons

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+var colours = [
+    ["#315f72", "#f57e7e"],
+    ["#33dbff", "#ff5733"],
+    ["#CC378D", "#37CC76"],
+    ["#718DEC", "#ECD071"],
+    ["#EAB5C8", "#B5EAD7"]
+]
+var randomNumber = Math.floor(Math.random() * (colours.length))
+
+// gets center text to cache
+var text = document.getElementById("centreText")
+
+//gets icons to cache
+var githubIcon = document.getElementById('githubIcon')
+var discordIcon = document.getElementById('discordIcon')
+
+var icons = [githubIcon, discordIcon]
+
 window.addEventListener("load", () => {
-    var colours = [
-        ["#315f72", "#f57e7e"],
-        ["#33dbff", "#ff5733"],
-        ["#CC378D", "#37CC76"],
-        ["#718DEC", "#ECD071"],
-        ["#EAB5C8", "#B5EAD7"]
-    ]
-    var randomNumber = Math.floor(Math.random() * (colours.length))
 
-    var text = document.getElementById("centreText")
-
+    // sets colours
     text.style.color = colours[randomNumber][1]
     document.body.style.background = colours[randomNumber][0]
+
+
+    icons.forEach((item, i) => {
+        //changes icon colour to text colour
+        item.style.color = colours[randomNumber][1]
+        item.style.transition = "all 500ms"
+    });
 })
+
+icons.forEach((item, i) => {
+    item.addEventListener("mouseenter", (event) => {
+        event.target.style.color = "#000000"
+        event.target.style.transition = "500ms"
+    })
+    item.addEventListener("mouseleave", event => {
+        event.target.style.color = colours[randomNumber][1]
+    })
+});
